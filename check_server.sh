fb4f08cc03825a6407f84956b1c38ad4b8523cc2
Benjamín Kosa <benkosa0@gmail.com>
2023-09-29 09:56:48 +0200

    check server

--- check_server.sh
+++ check_server.sh
@@ -3,5 +3,5 @@
 # Check if the server is running
 if ! curl -s http://158.180.53.241:3000 >/dev/null; then
     # If the server is not running, start it
-    npm run restart &
+    cd /home/opc/git/Bombear && npm run restart &
 fi
