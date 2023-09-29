20b61c187b76c73375aa817b70f73112e0cdbf01
Benjamín Kosa <benkosa0@gmail.com>
2023-09-29 10:12:07 +0200

    remove schedule

--- check_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Check if the server is running
-if ! curl -s http://158.180.53.241:3000 >/dev/null; then
-    # If the server is not running, start it
-    cd /home/opc/git/Bombear && npm run restart &
-fi
